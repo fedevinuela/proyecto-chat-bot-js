9db68df1f305d9688c743f6d0a7ba683c1c070dd
fvinuela <federico.vinuela@gmail.com>
2023-10-11 19:32:22 -0300

    actualización para entrega dos

--- main.js
+++ main.js
@@ -1,109 +1,184 @@
 for (let i=0; i<2;i++){  
-    // Objeto que almacenará la información del usuario
+  // Objeto que almacenará la información del usuario
   const usuario = {
     nombre: "",
     clase: "",
     turno: "",
     horario: "",
   };
-
+  
+  // Array de usuarios
+  const usuarios = [
+    {
+      nombreUsuario: "juanEjemplo",
+      contraseña: "1234",
+      nombre: "",
+      clase: "",
+      turno: "",
+      horario: "",
+    },
+    {
+      nombreUsuario: "CarlosPrueba",
+      contraseña: "1234",
+      nombre: "",
+      clase: "",
+      turno: "",
+      horario: "",
+    },
+    
+  ];
+  
   // Función para iniciar el chatbot
   function iniciarChatbot() {
     alert("Hola, bienvenido. Mi nombre es DolceBot y te voy a estar guiando para que puedas sacar tu turno de entrenamiento.");
-
-    // Pide al usuario su nombre
-    pedirNombre();
-  }
-
-  // Función para pedir el nombre del usuario
-  function pedirNombre() {
-    const nombre = prompt("Por favor, ingrese su nombre de usuario:");
-    usuario.nombre = nombre;
-
-    // Pide al usuario que seleccione la clase
-    alert(`Hola, ${nombre}. A continuación elija el número de la clase a la que quiere asistir`);
-
-    const claseSeleccionada = prompt("1: Open Box,    2: Sesion Fitnes,    3: Spining,    4: CrosFit");
-    usuario.clase = obtenerClasePorNumero(claseSeleccionada);
-
-    // Pide al usuario que seleccione el turno
-    alert("A continuación por favor, escoja el turno en el que desea asistir");
-    const turnoSeleccionado = prompt("1. Turno Mañana,    2. Turno Tarde");
-    usuario.turno = obtenerTurnoPorNumero(turnoSeleccionado);
-
-    // Pide al usuario que seleccione el horario
-    if (turnoSeleccionado == "1") {
-      alert("A continuación por favor, elija el horario en el que desea asistir");
-      const horarioTurnoManana = prompt("1. de 9 a 10,    2. de 10 a 11,    3. de 11 a 12");
-      usuario.horario = obtenerHorarioTm(horarioTurnoManana);
-      
-    } else if (turnoSeleccionado == "2") {
-      alert("A continuación por favor, elija el horario en el que desea asistir");
-      const horarioTurnoTarde = prompt("1. de 14 a 15,    2. de 15 a 16,    3. de 17 a 18");
-      usuario.horario = obtenerHorarioTt(horarioTurnoTarde);
+  
+    const opcion = prompt("¿Qué desea hacer?\n1. Iniciar sesión\n2. Registrarse");
+  
+    if (opcion === "1") {
+      const nombreUsuario = prompt("Ingrese su nombre de usuario:");
+      const contraseña = prompt("Ingrese su contraseña:");
+  
+      // Verifica si las credenciales son correctas
+      const usuarioEncontrado = usuarios.find(
+        (usuario) =>
+          usuario.nombreUsuario === nombreUsuario && usuario.contraseña === contraseña
+      );
+  
+      if (usuarioEncontrado) {
+        alert(`Bienvenido, ${nombreUsuario}!`);
+        usuario.nombre = nombreUsuario;
+  
+        // Continúa con el proceso de selección de clase, turno y horario
+        pedirTurno();
+      } else {
+        alert("Credenciales incorrectas. Inténtelo de nuevo o regístrese.");
+        iniciarChatbot();
+      }
+    } else if (opcion === "2") {
+      registrarUsuario();
+    } else {
+      alert("Opción no válida. Por favor, seleccione 1 para iniciar sesión o 2 para registrarse.");
+      iniciarChatbot();
     }
-
-    // Muestra la confirmación de registro
-    console.log ("\n¡Registro exitoso!\n" + `Nombre: ${usuario.nombre}\nClase: ${usuario.clase}\nTurno: ${usuario.turno}\nHorario: ${usuario.horario}`);
+  }
+  
+  // Función para registrar un nuevo usuario
+  function registrarUsuario() {
+    const nuevoUsuario = {
+      nombreUsuario: prompt("Ingrese un nombre de usuario:"),
+      contraseña: prompt("Ingrese una contraseña:"),
+      nombre: "",
+      clase: "",
+      turno: "",
+      horario: "",
+    };
+  
+    usuarios.push(nuevoUsuario);
+  
+    console.log("Nuevo usuario registrado:", nuevoUsuario); 
+  
+    alert("¡Registro exitoso! Ahora puede iniciar sesión.");
+  
+    iniciarChatbot();
+  
+  }
+  
+  // Función para pedir el turno del usuario
+  function pedirTurno() {
+    alert(`${usuario.nombre}, siga los siguientes pasos para registrar su clase!`);
     
+    // Agrega lógica para seleccionar la clase, el turno y el horario aquí
+    seleccionarClase();
+    seleccionarTurno();
+    seleccionarHorario();
+    
+    // Muestra la confirmación de registro
+    console.log("\n¡Registro exitoso!\n" + `Nombre: ${usuario.nombre}\nClase: ${usuario.clase}\nTurno: ${usuario.turno}\nHorario: ${usuario.horario}`);
     alert("\n¡Registro exitoso!\n" + `Nombre: ${usuario.nombre}\nClase: ${usuario.clase}\nTurno: ${usuario.turno}\nHorario: ${usuario.horario}`);
   }
-
-
-  // Funciones de utilidad para obtener opciones por número
-  function obtenerClasePorNumero(numero) {
-    switch (numero) {
-      case "1":
-        return "Open Box";
-      case "2":
-        return "Sesion Fitnes";
-      case "3":
-        return "Spining";
-      case "4":
-        return "Cros Fit";
-      default:
-        return "Clase no válida";
-    }
-  }
-
-  function obtenerTurnoPorNumero(numero) {
-    switch (numero) {
-      case "1":
-        return "Turno Mañana";
-      case "2":
-        return "Turno Tarde";
-      default:
-        return "Turno no válido";
-    }
-  }
-
-  function obtenerHorarioTm(numero) {
-    switch (numero) {
-      case "1":
-        return "9 a 10 hs";
-      case "2":
-        return "10 a 11 hs";
-      case "3":
-        return "11 a 12 hs";
-      default:
-        return "Horario no válido";
-    }
-  }
-
-  function obtenerHorarioTt(numero) {
-    switch (numero) {
-      case "1":
-        return "14 a 15 hs";
-      case "2":
-        return "15 a 16 hs";
-      case "3":
-        return "17 a 18 hs";
-      default:
-        return "Horario no válido";
-    }
-  }
-
+  
   // Iniciar el chatbot
   iniciarChatbot();
-
-}
+  
+  // Función para seleccionar la clase
+  function seleccionarClase() {
+    //seleccionar clase
+    const opcion = prompt("A continuación elija el número de la clase a la que quiere asistir\n 1: Open Box\n 2: Sesión Fitness\n 3: Spinning\n 4: CrossFit");
+  
+    switch (opcion) {
+      case "1":
+        usuario.clase = "Open Box";
+        break;
+      case "2":
+        usuario.clase = "Sesión Fitness";
+        break;
+      case "3":
+        usuario.clase = "Spinning";
+        break;
+      case "4":
+        usuario.clase = "CrossFit";
+        break;
+      default:
+        usuario.clase = "Clase no válida";
+    }
+  }
+  
+  // Función para seleccionar el turno
+  function seleccionarTurno() {
+    //Seleccion de turno
+    const opcion = prompt("A continuación, escoja el turno en el que desea asistir:\n 1: Turno Mañana\n 2: Turno Tarde");
+  
+    switch (opcion) {
+      case "1":
+        usuario.turno = "Turno Mañana";
+        break;
+      case "2":
+        usuario.turno = "Turno Tarde";
+        break;
+      default:
+        usuario.turno = "Turno no válido";
+    }
+  }
+  
+  // Función para seleccionar el horario
+  function seleccionarHorario() {
+    if (usuario.turno === "Turno Mañana") {
+    //seleccionar horario turno mañana
+      const opcion = prompt("A continuación, elija el horario en el que desea asistir en la mañana:\n 1: 9 a 10 hs\n 2: 10 a 11 hs\n 3: 11 a 12 hs");
+  
+      switch (opcion) {
+        case "1":
+          usuario.horario = "9 a 10 hs";
+          break;
+        case "2":
+          usuario.horario = "10 a 11 hs";
+          break;
+        case "3":
+          usuario.horario = "11 a 12 hs";
+          break;
+        default:
+          usuario.horario = "Horario no válido";
+      }
+    } else if (usuario.turno === "Turno Tarde") {
+  
+      const opcion = prompt("A continuación, elija el horario en el que desea asistir en la tarde:\n 1: 14 a 15 hs\n 2: 15 a 16 hs\n 3: 17 a 18 hs");
+  
+      switch (opcion) {
+        case "1":
+          usuario.horario = "14 a 15 hs";
+          break;
+        case "2":
+          usuario.horario = "15 a 16 hs";
+          break;
+        case "3":
+          usuario.horario = "17 a 18 hs";
+          break;
+        default:
+          usuario.horario = "Horario no válido";
+      }
+    }
+  }
+  
+  }
+  
+  
